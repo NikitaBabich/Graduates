d061c85e2f154b9a94c2c58929257594614f94c7
Никита Бабич <Babichnikita23@gmail.com>
2022-04-10 19:28:13 +0700

    Добавьте файлы проекта.

---
+++ Graduates/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Graduates
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GraduatesEntities context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new GraduatesEntities();
+            ShowTable();
+        }
+
+        private void ShowTable()
+        {
+            DataGridStorage.ItemsSource = context.Storage_objects.ToList();
+        }
+
+        private void BtnAddData_Click(object sender, RoutedEventArgs e)
+        {
+            var NewZap = new Storage_objects();
+            context.Storage_objects.Add(NewZap);
+            var EditWindow = new Windows.ObjectAddWindow(context, NewZap);
+            EditWindow.ShowDialog();
+            ShowTable();
+        }
+
+        private void BtnDeleteData_Click(object sender, RoutedEventArgs e)
+        {
+            var currentZap = DataGridStorage.SelectedItem as Storage_objects;
+            if (currentZap == null)
+            {
+                MessageBox.Show("Выберите строку!");
+                return;
+            }
+            MessageBoxResult messageBoxResult = MessageBox.Show("Вы хотите удалить?", "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                context.Storage_objects.Remove(currentZap);
+                context.SaveChanges();
+                MessageBox.Show("Данные удалены");
+                ShowTable();
+            }
+        }
+
+        private void BtnEditData_Click(object sender, RoutedEventArgs e)
+        {
+            Button BtnEdit = sender as Button;
+            var currentZap = BtnEdit.DataContext as Storage_objects;
+            var EditWindow = new Windows.ObjectAddWindow (context, currentZap);
+            EditWindow.ShowDialog();
+        }
+
+        private void BtnSelectStudent_Click(object sender, RoutedEventArgs e)
+        {
+            var StudentsList = new Windows.SudentsListWindow();
+            StudentsList.ShowDialog();
+        }
+
+        private void BtnSelectTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            var TeachersList = new Windows.TeachersListWindow();
+            TeachersList.ShowDialog();
+        }
+
+        private void BtnSelectGGroup_Click(object sender, RoutedEventArgs e)
+        {
+            var GroupsList = new Windows.GroupsListWindow();
+            GroupsList.ShowDialog();
+        }
+    }
+}
---
+++ Graduates/Windows/ObjectAddWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Graduates.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ObjectAddWindow.xaml
+    /// </summary>
+    public partial class ObjectAddWindow : Window
+    {
+        GraduatesEntities context;
+        public ObjectAddWindow(GraduatesEntities context, Object newobject)
+        {
+            InitializeComponent();
+            this.context = context;
+            CmbObject.ItemsSource = context.Type_storage_object.ToList();
+            CmbStudent.ItemsSource = context.Students.ToList();
+            CmbTeacher.ItemsSource = context.Teachers.ToList();
+            this.DataContext = newobject;
+        }
+        private void BtnSaveData_Click(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+            MessageBox.Show("Данные добавлены");
+            this.Close();
+        }
+    }
+}
